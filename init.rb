1445cd29b46451158198eaf4538c1150cbf3af54
Alexey Glukhov <gluhov1985@gmail.com>
2014-06-25 09:32:21 +1100

    #20262

--- init.rb
+++ init.rb
@@ -3,6 +3,6 @@ Redmine::Plugin.register :redmine_issue_tabs do
   author 'Pitin Vladimir, Alexey Glukhov'
   description 'Plugin enhances issue interface by adding several useful tabs: timelog, time spent, code commits, history'
   version '0.0.1'
-  url 'http://example.com/path/to/plugin'
+  url 'https://github.com/pineapple-thief/redmine_issue_tabs'
   author_url 'http://example.com/about'
 end
