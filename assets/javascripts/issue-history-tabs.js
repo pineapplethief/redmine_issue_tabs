e72191e1e2e57eab014843c45768749a500cd45f
Danil Kukhlevskiy <kukhlevskiy@gmail.com>
2014-10-08 19:28:40 +1100

    #25025

--- assets/javascripts/issue-history-tabs.js
+++ assets/javascripts/issue-history-tabs.js
@@ -1,64 +1,6 @@
 RMPlus.TABS = (function (my) {
   var my = my || {};
 
-  // my.call_func = function(func) {
-  //   if (my[func])
-  //     my[func].apply(this, Array.prototype.slice.call(arguments, 1));
-  //   else
-  //     my.hide_everything();
-  // };
-
-  // click handler for tabs
-  // my.click_handler = function() {
-  //   // getting tab name without 'tab-' prefix and building function name
-  //   var tab_name = this.id.substring(4, this.id.length);
-  //   var function_name = 'show_' + tab_name;
-  //   my.call_func(function_name);
-  // };
-
-  // my.show_comments = function () {
-  //   $('.journal').each(function (index) {
-  //     var $journal = $(this);
-  //     $journal.hide();
-  //     if ($journal.hasClass('has-notes')) {
-  //       $journal.show();
-  //     }
-  //     if ($journal.find('a[href^="/attachments"]').length > 0) {
-  //       $journal.show();
-  //       $journal.find('.details li').hide();
-  //       $journal.find('li:has(a[href^="/attachments"])').show();
-  //     } else {
-  //       $journal.find('ul.details').hide();
-  //     }
-  //   });
-  //   $('#issue_timelog').addClass('I');
-  //   $('#issue-changesets').addClass('I');
-  // };
-
-  // my.show_history = function () {
-  //   $('.journal').show().find('.details').show().find('li').show();
-  //   $('#issue_timelog').addClass('I');
-  //   $('#issue-changesets').addClass('I');
-  // };
-
-  // my.show_timelog = function() {
-  //   $('.journal').hide();
-  //   $('#issue_timelog').removeClass('I');
-  //   $('#issue-changesets').addClass('I');
-  // };
-
-  // my.show_changesets = function() {
-  //   $('.journal').hide();
-  //   $('#issue_timelog').addClass('I');
-  //   $('#issue-changesets').removeClass('I');
-  // };
-
-  // my.hide_everything = function() {
-  //   $('.journal').hide();
-  //   $('#issue_timelog').addClass('I');
-  //   $('#issue-changesets').addClass('I');
-  // };
-
   my.load_issue_view_stats = function(url, tab_header, show_tab) {
     var header_content = '<li>';
     header_content += '<a href="' + url + '?tab=view_stats" id="tab-view_stats" onclick="showTab(\'view_stats\', this.href); this.blur(); return false;" class="no_line in_link" data-remote="true"><span>' + tab_header + '</span></a>';
@@ -107,7 +49,7 @@ $(document).ready(function () {
   });
 
   if (has_timelog) {
-    $('#issue_timelog').appendTo($('#tab-content-timelog'));
+    $('#issue_timelog').appendTo('#tab-content-timelog');
   }
   else {
     $('#tab-timelog').remove();
@@ -115,7 +57,7 @@ $(document).ready(function () {
 
 
   if (has_history) {
-    $('#history').appendTo($('#tab-content-history'));
+    $('#history').appendTo('#tab-content-history');
   }
   else {
     $('#tab-history').remove();
@@ -129,7 +71,7 @@ $(document).ready(function () {
   }
 
   if (has_changesets) {
-    $('#issue-changesets').appendTo($('#tab-content-changesets'));
+    $('#issue-changesets').appendTo('#tab-content-changesets');
   }
   else {
     $('#tab-changesets').remove();
@@ -139,22 +81,12 @@ $(document).ready(function () {
     $('#history_tabs').remove();
   }
   else {
-    var tabs = $('div#history_tabs');
-    $('div#history_tabs').remove();
-    $('div.issue').after(tabs);
+    $('#history_tabs').insertAfter('div.issue:first')
   }
 
   if ($('.tabs a.selected').length == 0){
     $('.tabs a').first().addClass('selected');
   }
-
-  // $('div#content .tabs a.selected').each( function() {
-  //   RMPlus.TABS.click_handler.apply(this);
-  // });
-
-  // $(document.body).on('click', 'div#content .tabs a', function(event){
-  //   RMPlus.TABS.click_handler.apply(this);
-  // });
 
   $('.tabs-buttons').hide();
 });
