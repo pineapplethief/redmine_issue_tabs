fd0c42a887bcfaf3974f24a50ac9e80d3c230a4a
Павел Шепель <tacid@tacid.kiev.ua>
2016-05-18 17:37:17 +0300

    Update to 1.1.1 Changelog   1.1.1:     * fix double tabs in issues page   1.1.0:     * support Redmine 3.0     * fixed rights for timelog page

--- init.rb
+++ init.rb
@@ -2,7 +2,7 @@ Redmine::Plugin.register :redmine_issue_tabs do
   name 'Redmine Issue Tabs plugin'
   author 'Pitin Vladimir, Alexey Glukhov'
   description 'Plugin enhances issue interface by adding several useful tabs: timelog, time spent, code commits, history'
-  version '0.0.1'
+  version '1.1.1'
   url 'https://github.com/pineapple-thief/redmine_issue_tabs'
   author_url 'http://example.com/about'
 end
